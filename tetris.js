d26471126e18d33b99098003bcbf56ea028bbe64
mastrmind77 <iancanfield@mail.com>
2021-02-09 00:14:42 -0600

    touchy?

--- tetris.js
+++ tetris.js
@@ -473,10 +473,11 @@ var mc = new Hammer(myElement);
 
 // let the pan gesture support all directions.
 // this will block the vertical scrolling on a touch-device while on the element
-mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+//mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
 
 // listen to events...
-mc.on("panleft panright panup pandown tap press", function(ev) {
+mc.on("panleft panright tap press", function(ev) {
+//mc.on("panleft panright panup pandown tap press", function(ev) {
     // myElement.textContent = ev.type +" gesture detected.";
     console.log(ev);
 
